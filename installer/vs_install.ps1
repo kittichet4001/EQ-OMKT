2db1819b37b2bc1463008d1cdc023ddff4903679
kittichet4001 <omexo4001@gmail.com>
2026-05-29 17:39:28 +0700

    edit vs installer file

--- installer/vs_install.ps1
+++ installer/vs_install.ps1
@@ -1,6 +1,1 @@
-# บังคับเช็คและขอสิทธิ์ Admin ตั้งแต่บรรทัดแรก
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    Exit
-}
-winget install --name "Visual Studio Community 2026" --override "--passive --config .\custom_pkg_vs_installer.json"
+winget install --name "Visual Studio Community 2026" --override "--passive --config .\scripts\custom_pkg_vs_installer.json"
